3da079df2013e9438cde23ac49f488b1cd90ddb0
Matthew Lock <matthew_lock20@hotmail.com>
2020-10-13 18:34:48 +0100

    Keys added to todos

--- src/NoteList.js
+++ src/NoteList.js
@@ -3,18 +3,18 @@ import React, { Component } from "react";
 class NoteList extends Component {
 constructor(props) {
   super(props)
-  this.state = { value: '', todo: [], checked: false }
+  this.state = { todo: [], currentItem:{ value: '', key:'' }, checked: false }
   
 this.handleChange = this.handleChange.bind(this);
 this.handleSubmit = this.handleSubmit.bind(this);
 }
 
  handleChange(event) {
-   this.setState({value: event.target.value});
+   this.setState({currentItem:{value: event.target.value, key:Date.now()}});
  }
 
  handleSubmit(event) {
-   this.state.todo.push(this.state.value)
+   this.state.todo.push(this.state.currentItem.value)
    this.setState({todo: this.state.todo});
    this.setState({...this.setState, value: ''})
    event.preventDefault();
@@ -36,10 +36,10 @@ render() {
     <div className = 'Personal NoteList'>
     <form id= 'todo-form' onSubmit = {this.handleSubmit}>
       <label>
-        Task: <input type='text' value={this.state.value}onChange = {this.handleChange}/>
+        Task: <input type='text' value={this.state.currentItem.value}onChange = {this.handleChange}/>
       </label>
       <button type = 'submit'>Add</button>
-      <button type = 'submit' onClick ={this.resetForm}>Reset</button>
+      <button type = 'subm it' onClick ={this.resetForm}>Reset</button>
       </form>
       
     You have {this.state.todo.length} Tasks
